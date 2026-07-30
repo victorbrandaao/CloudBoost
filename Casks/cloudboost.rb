49b17ba4436cbdd8cd1ce5c8853a4efe330c9eb4
Victor Brandão <victorbrandaotech@gmail.com>
2026-07-30 11:57:28 -0300

    chore: align Homebrew cask style

--- Casks/cloudboost.rb
+++ Casks/cloudboost.rb
@@ -5,10 +5,11 @@ cask "cloudboost" do
   url "https://github.com/victorbrandaao/CloudBoost/releases/download/v#{version}/CloudBoost_v#{version}.dmg",
       verified: "github.com/victorbrandaao/CloudBoost/"
   name "CloudBoost"
-  desc "macOS menu bar app for cloud and Mac gaming session diagnostics"
+  desc "Menu bar app for cloud gaming and play-session diagnostics"
   homepage "https://getcloudboost.site/"
 
   depends_on macos: :monterey
+
   app "CloudBoost.app"
 
   zap trash: [
